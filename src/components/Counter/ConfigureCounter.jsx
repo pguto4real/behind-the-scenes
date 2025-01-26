fd309907e22a70084c6bd5779152790fd01f4398
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-25 22:30:12 -0600

    using clever structuring

--- src/components/Counter/ConfigureCounter.jsx
+++ src/components/Counter/ConfigureCounter.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { log } from "../../log";
 
-const ConfigureCounter = () => {
+const ConfigureCounter = ({ onSet }) => {
+  log("<ConfigureCounter />", 1);
+  const [enteredNumber, setEnteredNumber] = useState(0);
+  function handleChange(event) {
+    setEnteredNumber(+event.target.value);
+  }
+
+  function handleSetClick() {
+    onSet(enteredNumber);
+    setEnteredNumber(0);
+  }
+
   return (
-    <div>ConfigureCounter</div>
-  )
-}
+    <section id="configure-counter">
+      <h2>Set Counter</h2>
+      <input type="number" onChange={handleChange} value={enteredNumber} />
+      <button onClick={handleSetClick}>Set</button>
+    </section>
+  );
+};
 
-export default ConfigureCounter
+export default ConfigureCounter;
