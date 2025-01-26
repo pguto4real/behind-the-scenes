97cb7273c02c8f15ee6c41e0a56ed1ae6ad5c46c
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-25 22:30:26 -0600

    removing memo from counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,17 @@
-import { useState,memo } from "react";
+import { useState } from 'react';
 
-import IconButton from "../UI/IconButton.jsx";
-import MinusIcon from "../UI/Icons/MinusIcon.jsx";
-import PlusIcon from "../UI/Icons/PlusIcon.jsx";
-import CounterOutput from "./CounterOutput.jsx";
-import { log } from "../../log.js";
+import IconButton from '../UI/IconButton.jsx';
+import MinusIcon from '../UI/Icons/MinusIcon.jsx';
+import PlusIcon from '../UI/Icons/PlusIcon.jsx';
+import CounterOutput from './CounterOutput.jsx';
+import { log } from '../../log.js';
 
 function isPrime(number) {
-  log("Calculating if is prime number", 2, "other");
+  log(
+    'Calculating if is prime number',
+    2,
+    'other'
+  );
   if (number <= 1) {
     return false;
   }
@@ -23,8 +27,8 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo(function Counter({ initialCount }) {
-  log("<Counter /> rendered", 1);
+export default function Counter({ initialCount }) {
+  log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
@@ -40,8 +44,8 @@ const Counter = memo(function Counter({ initialCount }) {
   return (
     <section className="counter">
       <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{" "}
-        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
       </p>
       <p>
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
@@ -54,5 +58,4 @@ const Counter = memo(function Counter({ initialCount }) {
       </p>
     </section>
   );
-});
-export default Counter;
+}
