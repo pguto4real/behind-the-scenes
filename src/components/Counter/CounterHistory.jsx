cd2027f06da8942b86be16a187cd33b9afbde9b6
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-26 23:22:30 -0600

    remove history item from counter history

--- src/components/Counter/CounterHistory.jsx
+++ src/components/Counter/CounterHistory.jsx
@@ -2,21 +2,7 @@ import { useState } from 'react';
 
 import { log } from '../../log.js';
 
-function HistoryItem({ count }) {
-  log('<HistoryItem /> rendered', 3);
 
-  const [selected, setSelected] = useState(false);
-
-  function handleClick() {
-    setSelected((prevSelected) => !prevSelected);
-  }
-
-  return (
-    <li onClick={handleClick} className={selected ? 'selected' : undefined}>
-      {count}
-    </li>
-  );
-}
 
 export default function CounterHistory({ history }) {
   log('<CounterHistory /> rendered', 2);
