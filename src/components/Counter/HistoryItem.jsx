2db22758750e6d1e55a441f4cb395337f551ba5c
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-26 23:21:45 -0600

    HistoryItem data

--- src/components/Counter/HistoryItem.jsx
+++ src/components/Counter/HistoryItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const HistoryItem = () => {
-  return (
-    <div>HistoryItem</div>
-  )
+const HistoryItem = ({ count }) => {
+  log('<HistoryItem /> rendered', 3);
+  
+    const [selected, setSelected] = useState(false);
+  
+    function handleClick() {
+      setSelected((prevSelected) => !prevSelected);
+    }
+  
+    return (
+      <li onClick={handleClick} className={selected ? 'selected' : undefined}>
+        {count}
+      </li>
+    );
 }
 
 export default HistoryItem
