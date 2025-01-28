fb0455c7968bb56f5bcab6671825e59f904d1a12
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-27 21:25:30 -0600

    keys

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,6 +5,7 @@ import MinusIcon from "../UI/Icons/MinusIcon.jsx";
 import PlusIcon from "../UI/Icons/PlusIcon.jsx";
 import CounterOutput from "./CounterOutput.jsx";
 import { log } from "../../log.js";
+import CounterHistory from "./CounterHistory.jsx";
 
 function isPrime(number) {
   log("Calculating if is prime number", 2, "other");
@@ -31,19 +32,28 @@ export default function Counter({ initialCount }) {
   );
 
   // const [counter, setCounter] = useState(initialCount);
-  const [counterChanges, setCounterChanges] = useState([initialCount]);
+  const [counterChanges, setCounterChanges] = useState([
+    { value: initialCount, id: Math.random() * 1000 },
+  ]);
 
   const currentCounter = counterChanges.reduce(
-    (prevCounter, counterChange) => prevCounter + counterChange,0
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
   );
 
   const handleDecrement = useCallback(function handleDecrement() {
     // setCounter((prevCounter) => prevCounter - 1);
-    setCounterChanges((prevCounter) => [-1, ...prevCounter]);
+    setCounterChanges((prevCounter) => [
+      { value: -1, id: Math.random() * 1000 },
+      ...prevCounter,
+    ]);
   }, []);
   const handleIncrement = useCallback(function handleIncrement() {
     // setCounter((prevCounter) => prevCounter + 1);
-    setCounterChanges((prevCounter) => [1, ...prevCounter]);
+    setCounterChanges((prevCounter) => [
+      { value: 1, id: Math.random() * 1000 },
+      ...prevCounter,
+    ]);
   }, []);
 
   return (
@@ -61,6 +71,7 @@ export default function Counter({ initialCount }) {
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 }
