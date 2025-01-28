2683fddc10ae6cff47a73a19c9b79214c92a624e
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-27 21:29:54 -0600

    use key to reset state

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        <Counter initialCount={chosenCount} key={chosenCount} />
         <Counter initialCount={0} />
       </main>
     </>
